513558b91d5d005e4a1eb2cff74fb75af013f152
Laurie Gauthier <elpassa339@gmail.com>
2026-02-09 17:58:26 +0100

    Step 2 : Home page and basics

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findBy(
+            ['isPublished' => true],
+            ['createdAt' => 'DESC'],
+            9
+        );
+
+        return $this->render('home.html.twig', [
+            'products' => $products,
+        ]);
+    }
+}
